fbf372bd5ab1ff8f30b5c2ff158cbb598b454975
Arjesh <arjesh.khadka2@gmail.com>
2023-09-04 20:39:24 +1000

    rest api

--- routes/feedback.js
+++ routes/feedback.js
@@ -4,6 +4,13 @@ const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
 
+const validations = [
+  check('name').trim().isLength({ min: 3 }).escape().withMessage('A name is required'),
+  check('email').trim().isEmail().normalizeEmail().withMessage('A email is required'),
+  check('title').trim().isLength({ min: 3 }).escape().withMessage('A title is required'),
+  check('message').trim().isLength({ min: 5 }).escape().withMessage('A message is required'),
+];
+
 module.exports = (params) => {
   const { feedbackService } = params;
 
@@ -27,15 +34,8 @@ module.exports = (params) => {
     }
   });
 
-  router.post(
-    '/',
-    [
-      check('name').trim().isLength({ min: 3 }).escape().withMessage('A name is required'),
-      check('email').trim().isEmail().normalizeEmail().withMessage('A email is required'),
-      check('title').trim().isLength({ min: 3 }).escape().withMessage('A title is required'),
-      check('message').trim().isLength({ min: 5 }).escape().withMessage('A message is required'),
-    ],
-    async (req, res) => {
+  router.post('/', validations, async (req, res, next) => {
+    try {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
         req.session.feedback = {
@@ -50,8 +50,25 @@ module.exports = (params) => {
         message: 'Thank you for you feedback',
       };
       return res.redirect('/feedback');
+    } catch (error) {
+      return next(error);
     }
-  );
+  });
+
+  router.post('/api', validations, async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.json({ errors: errors.array() });
+      }
+      const { name, email, title, message } = req.body;
+      await feedbackService.addEntry(name, email, title, message);
+      const feedback = await feedbackService.getList();
+      return res.json({ feedback, successMessage: 'Thank you for your feedback' });
+    } catch (error) {
+      return next(error);
+    }
+  });
 
   return router;
 };
